fb888d6529dc662d78def8158deed2acfcaf758a
rishan-mohamed <rishan@slingmobility.com>
2023-11-28 14:28:04 +0530

    init

---
+++ src/content_option.js
@@ -0,0 +1,136 @@
+const logotext = "Rishan";
+const meta = {
+    title: "Mohamed Rishan",
+    description: "Welcome to the website of Mohamed Rishan, a Flutter developer and full-stack developer currently working at Sling Mobility. Explore my portfolio and projects.",
+};
+
+const introdata = {
+    title: "I’m Mohamed Rishan",
+    animated: {
+        first: "I love coding",
+        second: "I code cool Mobile apps",
+        third: "I love develop mobile apps",
+    },
+    description: "Welcome to the website of Mohamed Rishan, a Flutter developer and full-stack developer currently working at Sling Mobility. Explore my portfolio and projects.",
+    your_img_url: "https://media.licdn.com/dms/image/D5603AQFgttMj2jMZ-Q/profile-displayphoto-shrink_800_800/0/1698740145570?e=1705536000&v=beta&t=fMYT85DnwSiPJEBt5wdlVFoat-rMyJxwyVZRJNrLKWU",
+};
+
+
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "I'm a self-taught Mobile application developer with 6 months of experience, focusing on Flutter. I enjoy creating user-friendly interfaces and high-performance apps. I like working in teams and contributing to project objectives. I'm also keen on learning and have experience with technologies like Flutter and Firebase.. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+};
+const worktimeline = [{
+        jobtitle: "Flutter developer",
+        where: "Sling Mobility",
+        date: "May2023",
+    },
+    {
+        jobtitle: "Jounior Oparetion executive",
+        where: "Sling Mobility",
+        date: "Nov 2022-May2023",
+    },
+    {
+        jobtitle: "Oparetion Assitance",
+        where: " A2Z infraservices lanka",
+        date: "Dec 2020-Jan2022",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 20,
+    },
+    {
+        name: "dart",
+        value: 95,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 30,
+    },
+   
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const dataportfolio = [
+    
+    
+    {
+        img: "../../assets/images/IMG-20230516-WA0034.jpg",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+ 
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "mohamedrishan224ggmail.com",
+    YOUR_FONE: "(+94)740700809",
+    description: "Feel free to reach out for professional inquiries or collaboration opportunities. I am available for discussions and networking.",
+   
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+
+const socialprofils = {
+    github: "https://github.com",
+    instagram: "https://instagram.com",   
+    linkedin: "https://linkedin.com/in/rishanmohamed2",
+    twitter: "https://x.com/M91663769Rishan?t=yD6zLrlbO6fd7rjyfkTWFQ&s=08",
+};
+
+
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
